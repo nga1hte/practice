9f1fb6f9d9620ed16632eb8a0ac4a40e101c21ef
Thangsuanlian Ngaihte <ngaihte@outlook.in>
2020-04-27 15:12:43 +0530

    Added new program isprime.c

---
+++ isprime.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int isprime(int n){
+    for(int i = 2; i < sqrt(n); i++){
+        if(n%i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+int main(){
+    int n;
+    printf("Please enter the number: ");
+    scanf("%d", &n);
+
+    if(isprime(n))
+        printf("The number %d is prime.\n", n);
+    else
+        printf("The number %d is not prime.\n", n);
+    
+}
